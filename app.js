1921c4e394c44d905b00d29a0d86fe6d31b71afb
Mahardhika Pratama <putrad578@gmail.com>
2023-02-14 21:54:51 +0700

    refactor: operator logic AND

--- app.js
+++ app.js
@@ -44,13 +44,18 @@
 const password = prompt('buat password!');
 
 //password harus sepanjang 6 karakter
-if(password.length >= 6) {
-    if(password.indexOf(' ') === -1) {
-        console.log('Password valid');
-    } else {
-        console.log('Password tidak boleh ada spasi');
-    }
-} else {
-    console.log('Password minimal 6 karakter');
-}
+// if(password.length >= 6) {
+//     if(password.indexOf(' ') === -1) {
+//         console.log('Password valid');
+//     } else {
+//         console.log('Password tidak boleh ada spasi');
+//     }
+// } else {
+//     console.log('Password minimal 6 karakter');
+// }
 
+if(password.length >= 6 && password.indexOf(' ') === -1) {
+    console.log('Password Valid');
+} else {
+    console.log('Password tidak memenuhi syarat');
+}
