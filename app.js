98ff1d76671a8ac5af679a4828594e0a91890184
Mahardhika Pratama <putrad578@gmail.com>
2023-02-14 21:04:41 +0700

    refactor: else if statement untuk banyak pilihan

--- app.js
+++ app.js
@@ -4,13 +4,35 @@
 // }
 // console.log('setelah pembanding');
 
-let angka = Math.random();
-console.log(angka);
+// let angka = Math.random();
+// console.log(angka);
 
-if(angka >= 0.5) {
-    console.log('Angka lebih besar dari 0.5');
-}
+// if(angka >= 0.5) {
+//     console.log('Angka lebih besar dari 0.5');
+// }
 
-if(angka <= 0.5) {
-    console.log('Angka lebih kecil dari 0.5');
+// if(angka <= 0.5) {
+//     console.log('Angka lebih kecil dari 0.5');
+// }
+
+// const iniHari = 'Sabtu';
+
+// if(iniHari === 'Senin') {
+//     console.log("Selamat bekerja kawan!");
+// } else if(iniHari === 'Sabtu') {
+//     console.log("Semoga jangan cepat berlalu!");
+// }
+
+const nilai = '49';
+
+if (nilai < 50) {
+    console.log("E");
+} else if (nilai < 60) {
+    console.log("D");
+} else if (nilai < 70) {
+    console.log("C");
+} else if (nilai < 80) {
+    console.log("B");
+} else if (nilai < 100) {
+    console.log("A");
 }
