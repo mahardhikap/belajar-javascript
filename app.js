5ff0b2c15e21f6d22afabcb559281f6fcdf24c79
Mahardhika Pratama <putrad578@gmail.com>
2023-02-26 21:07:41 +0700

    refactor: mencoba format pengulangan atau loop pada javascript

--- app.js
+++ app.js
@@ -68,14 +68,30 @@
 //     console.log('boleh mengaksesnya');
 // }
 
-const balonku = prompt('input warna balon')
-switch(balonku) {
-    case 'merah':
-        console.log('warna merah');
-        break;
-    case 'hijau':
-        console.log('dooooorrrrr');
-        break;
-    default:
-        console.log('bukan balon saya');
+// const balonku = prompt('input warna balon')
+// switch(balonku) {
+//     case 'merah':
+//         console.log('warna merah');
+//         break;
+//     case 'hijau':
+//         console.log('dooooorrrrr');
+//         break;
+//     default:
+//         console.log('bukan balon saya');
+// }
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+// console.log(8)
+// console.log(9)
+// console.log(10)
+
+for (let i = 1; i <= 10; i++) {
+    console.log('ini namanya loop');
+    console.log(i);
 }
