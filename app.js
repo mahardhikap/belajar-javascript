ae2e3f099245c0aee162600c14a13838dabfd484
Mahardhika Pratama <putrad578@gmail.com>
2023-02-26 21:19:22 +0700

    refactor: loop +2 dari 1, maka akan memunculkan nilai ganjil di loop

--- app.js
+++ app.js
@@ -91,7 +91,12 @@
 // console.log(9)
 // console.log(10)
 
-for (let i = 1; i <= 10; i++) {
+// for (let i = 1; i <= 10; i++) {
+//     console.log('ini namanya loop');
+//     console.log(i);
+// }
+
+for (let i = 1; i <= 20; i += 2) {
     console.log('ini namanya loop');
     console.log(i);
 }
