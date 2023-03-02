c6b400a8b3e8a48f6c37e6efdd40d3c0cdcd2ebc
Mahardhika Pratama <putrad578@gmail.com>
2023-03-02 16:53:54 +0700

    refactor: penggunaan function pada javascript untuk mempermudah dalam pemanggilan data

--- app.js
+++ app.js
@@ -255,5 +255,7 @@
 // }
 // console.log(total / scores.length);
 
-let lemparDadu = Math.floor(Math.random() * 6) + 1;
-console.log(lemparDadu);
+// let lemparDadu = console.log(lemparDadu);
+function lemparDadu() {
+    console.log(Math.floor(Math.random() * 6) + 1);
+}
