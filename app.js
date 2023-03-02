2db7b16eda36e001d122b77bba384f332484d5d6
Mahardhika Pratama <putrad578@gmail.com>
2023-03-02 16:49:02 +0700

    refactor: membuat koding javascript lempar dadu

--- app.js
+++ app.js
@@ -231,26 +231,29 @@
 // }
 
 
-const studentScore = {
-    Olivia: 20,
-    Liam: 13,
-    Emma: 40,
-    Noah: 18,
-    Amelia: 32,
-    Oliver: 10,
-    Ava: 11,
-    Elijah: 21,
-    Sophia: 14,
-    Mateo: 22,
-};
+// const studentScore = {
+//     Olivia: 20,
+//     Liam: 13,
+//     Emma: 40,
+//     Noah: 18,
+//     Amelia: 32,
+//     Oliver: 10,
+//     Ava: 11,
+//     Elijah: 21,
+//     Sophia: 14,
+//     Mateo: 22,
+// };
 
 // for (let student in studentScore) {
 //     console.log(`${student} memiliki skor ${studentScore[student]}`);
 // }
 
-let total = 0;
-let scores = Object.values(studentScore);
-for (let score of scores) {
-    total += score;
-}
-console.log(total / scores.length);
+// let total = 0;
+// let scores = Object.values(studentScore);
+// for (let score of scores) {
+//     total += score;
+// }
+// console.log(total / scores.length);
+
+let lemparDadu = Math.floor(Math.random() * 6) + 1;
+console.log(lemparDadu);
