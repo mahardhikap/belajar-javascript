ea678663406be99110b57f9b32fcf223ffc6b314
Mahardhika Pratama <putrad578@gmail.com>
2023-03-03 21:38:02 +0700

    refactor: fungsi method map, yaitu untuk mapping pada array, sehingga bisa mengumpulkan array dan mengalikan sesuai posisinya

--- app.js
+++ app.js
@@ -368,7 +368,10 @@
 //   }
 // }
 
-// const angka = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const angka = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const angkaDouble = angka.map(function(num) {
+    return num * 2;
+})
 
 // angka.forEach(function (el) {
 //   if (el % 2 === 0) {
@@ -376,29 +379,29 @@
 //   }
 // });
 
-const animes = [
-  {
-    title: "Attack On Titan",
-    rating: 90,
-  },
-  {
-    title: "One Piece",
-    rating: 89,
-  },
-  {
-    title: "Bleach",
-    rating: 88,
-  },
-  {
-    title: "Hunter x Hunter",
-    rating: 50,
-  },
-  {
-    title: "Naruto",
-    rating: 80,
-  },
-];
+// const animes = [
+//   {
+//     title: "Attack On Titan",
+//     rating: 90,
+//   },
+//   {
+//     title: "One Piece",
+//     rating: 89,
+//   },
+//   {
+//     title: "Bleach",
+//     rating: 88,
+//   },
+//   {
+//     title: "Hunter x Hunter",
+//     rating: 50,
+//   },
+//   {
+//     title: "Naruto",
+//     rating: 80,
+//   },
+// ];
 
-animes.forEach(function (anime) {
-  console.log(`${anime.title} - ${anime.rating}/100`);
-});
+// animes.forEach(function (anime) {
+//   console.log(`${anime.title} - ${anime.rating}/100`);
+// });
