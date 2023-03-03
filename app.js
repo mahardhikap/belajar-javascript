df0699f4928372200ae7a5d22e6c156ada1b0282
Mahardhika Pratama <putrad578@gmail.com>
2023-03-03 21:42:49 +0700

    refactor: penggunaan method map lainnya

--- app.js
+++ app.js
@@ -368,10 +368,10 @@
 //   }
 // }
 
-const angka = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const angkaDouble = angka.map(function(num) {
-    return num * 2;
-})
+// const angka = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const angkaDouble = angka.map(function(num) {
+//     return num * 2;
+// })
 
 // angka.forEach(function (el) {
 //   if (el % 2 === 0) {
@@ -379,29 +379,33 @@ const angkaDouble = angka.map(function(num) {
 //   }
 // });
 
-// const animes = [
-//   {
-//     title: "Attack On Titan",
-//     rating: 90,
-//   },
-//   {
-//     title: "One Piece",
-//     rating: 89,
-//   },
-//   {
-//     title: "Bleach",
-//     rating: 88,
-//   },
-//   {
-//     title: "Hunter x Hunter",
-//     rating: 50,
-//   },
-//   {
-//     title: "Naruto",
-//     rating: 80,
-//   },
-// ];
+const animes = [
+  {
+    title: "Attack On Titan",
+    rating: 90,
+  },
+  {
+    title: "One Piece",
+    rating: 89,
+  },
+  {
+    title: "Bleach",
+    rating: 88,
+  },
+  {
+    title: "Hunter x Hunter",
+    rating: 50,
+  },
+  {
+    title: "Naruto",
+    rating: 80,
+  },
+];
 
 // animes.forEach(function (anime) {
 //   console.log(`${anime.title} - ${anime.rating}/100`);
 // });
+
+const animeList = animes.map(function(anime) {
+    return anime.title.toUpperCase();
+})
