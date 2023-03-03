3dc5d859fccd87e62621853043dd5138094fb341
Mahardhika Pratama <putrad578@gmail.com>
2023-03-03 20:56:03 +0700

    refactor: callback function menggunakan forEach untuk data array

--- app.js
+++ app.js
@@ -370,8 +370,8 @@
 
 const angka = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-function print(element) {
-  console.log(element);
-}
-
-angka.forEach(print)
+angka.forEach(function (el) {
+  if (el % 2 === 0) {
+    console.log(el);
+  }
+});
