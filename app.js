d82a683bb8940af4a810c11b002f0326335af49d
Mahardhika Pratama <putrad578@gmail.com>
2023-03-03 21:06:32 +0700

    refactor: contoh lain callback function dengan forEach menggunakan data array

--- app.js
+++ app.js
@@ -368,10 +368,37 @@
 //   }
 // }
 
-const angka = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const angka = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-angka.forEach(function (el) {
-  if (el % 2 === 0) {
-    console.log(el);
-  }
+// angka.forEach(function (el) {
+//   if (el % 2 === 0) {
+//     console.log(el);
+//   }
+// });
+
+const animes = [
+  {
+    title: "Attack On Titan",
+    rating: 90,
+  },
+  {
+    title: "One Piece",
+    rating: 89,
+  },
+  {
+    title: "Bleach",
+    rating: 88,
+  },
+  {
+    title: "Hunter x Hunter",
+    rating: 50,
+  },
+  {
+    title: "Naruto",
+    rating: 80,
+  },
+];
+
+animes.forEach(function (anime) {
+  console.log(`${anime.title} - ${anime.rating}/100`);
 });
